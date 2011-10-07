5a8b7cba3946a445080d6b62208e85ef7534cffb
Paulo Machado <paulo.machado@tool.com.br>
2011-10-07 08:42:57 -0300

    Validation algorithm

--- lunender.m
+++ lunender.m
@@ -1,20 +1,14 @@
+## -*- texinfo -*-
+## @deftypefn  {Function File} {} lunender (@var{qtd_ped}, @var{qtd_est}, @var{atend_min}, @var{atend_max})
+## dado vetores de qtd_ped (quantidade de pedidos) e qtd_est(quantidade em estoque),
+## e porcentagem de atend_min e atend_max
+
 function lunender (qtd_ped, qtd_est, atend_min, atend_max)
 
-  % validacao lunender
-  % clear all;
-  %atend_min = 60;
-  %atend_max = 80;
-  %qtd_ped=[50 100 100 50];
-  %qtd_est=[30 70 100 200];
-
-  %qtd_ped=[100 200 200 300];
-  %qtd_est=[40 100 50 100];
-
-  %qtd_ped=[20 40 20];
-  %qtd_est=[80 100 60];
-
-  %qtd_ped=[20 40 20 30 90 10 35];
-  %qtd_est=[80 100 60 30 58 30 37];
+  if length(qtd_est)!=length(qtd_ped)
+    printf("vetor de pedidos tem tamanho diferente de vetor de estoque\n");
+    break
+  endif
 
   % calc grade
   l = length(qtd_ped);
@@ -65,6 +59,9 @@ function lunender (qtd_ped, qtd_est, atend_min, atend_max)
     if atend < atend_min
       printf("Atendimento(%d) menor que atendimento minimo(%d)\n", atend, atend_min);
       printf("Status: Pedido em ESPERA\n\n");
+    elseif atend > atend_max
+      printf("Atendimento(%d) maior que atendimento maximo(%d)\n", atend, atend_max);
+      printf("Status: Pedido em ESPERA\n\n");
     else
       printf("Status: Pedido ATENDIDO PARCIALMENTE\n");
       printf("O pedido é: ");
@@ -79,14 +76,19 @@ function lunender (qtd_ped, qtd_est, atend_min, atend_max)
   if A == true;
     printf("Status: Pedido COMPLETAMENTE ATENDIDO\n\n");
   else
-    printf("Status: Pedido ATENDIDO PARCIALMENTE\n");
-    printf("O pedido é: ");
-    qtd_ped
-    printf("Sera atendido: ");
     for i=1:l
       qtd_atend(i)=min(qtd_est(i),qtd_ped(i));
     endfor
-    qtd_atend
+    if mean(qtd_atend./qtd_ped) > atend_max
+      printf("Atendimento(%d) maior que atendimento maximo(%d)\n", atend, atend_max);
+      printf("Status: Pedido em ESPERA\n\n");
+    else
+      printf("Status: Pedido ATENDIDO PARCIALMENTE\n");
+      printf("O pedido é: ");
+      qtd_ped
+      printf("Sera atendido: ");
+      qtd_atend
+    endif
   endif
   printf("================================================\n\n");
 
