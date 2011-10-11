52b594be99877b0a1df74127fffa954f4144650d
Paulo Machado <paulo.machado@tool.com.br>
2011-10-11 06:49:14 -0700

    Added proto queries

--- lunender.m
+++ lunender.m
@@ -2,6 +2,11 @@
 ## @deftypefn  {Function File} {} lunender (@var{qtd_ped}, @var{qtd_est}, @var{atend_min}, @var{atend_max})
 ## dado vetores de qtd_ped (quantidade de pedidos) e qtd_est(quantidade em estoque),
 ## e porcentagem de atend_min e atend_max
+## qtd_ped:
+##  SELECT t.pedite_quantidade FROM tool_pedido_item t WHERE t.id_pedido = :id_pedido
+## qtd_est:
+##  SELECT t.pec_wms_saldo FROM tool_peca t WHERE (SELECT a.id_pedido FROM tool_pedido_item
+##    WHERE a.id_peca = t.id_peca) = :id_pedido
 
 function lunender (qtd_ped, qtd_est, atend_min, atend_max)
 
